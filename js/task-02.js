b5d06bc13e934466ab7fae9c44afe5f3449d809d
Roman Hudz <hudzrom@gmail.com>
2023-07-17 22:14:14 +0300

    Update_task_02

--- js/task-02.js
+++ js/task-02.js
@@ -1,14 +1,16 @@
 const ingredients = [
-  "Potatoes",
-  "Mushrooms",
-  "Garlic",
-  "Tomatos",
-  "Herbs",
-  "Condiments",
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
 ];
 
-for (const ingredient of ingredients) {
-  const liEl = document.createElement("li");
+const ingredientsList = document.getElementById('ingredients');
+
+ingredients.forEach((ingredient) => {
+  const liEl = document.createElement('li');
   liEl.textContent = ingredient;
-  document.getElementById("ingredients").appendChild(liEl);
-}
+  ingredientsList.append(liEl);
+});
